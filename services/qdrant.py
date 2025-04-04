00f932e8d697318a0f7ecdda3df0a166b632b007
Alberto Zuin <alberto.zuin@colossusbets.com>
2025-04-04 21:40:31 +0100

    Update Bedrock to use titan-embed-text-v2 model
    Switched from amazon.titan-embed-text-v1 to amazon.titan-embed-text-v2. This change ensures compatibility with the latest model version and improves embedding performance.

--- services/qdrant.py
+++ services/qdrant.py
@@ -3,7 +3,7 @@ import os
 import requests
 
 # QDRANT_URL = os.getenv("QDRANT_URL")
-QDRANT_URL = "http://172.16.1.228:6333"
+QDRANT_URL = "http://172.16.1.188:6333"
 
 def store_to_qdrant(id_: str, vector: list):
     payload = {
